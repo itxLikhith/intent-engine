b5bcd20b8ae4a3e5112964923b428920e64f2d03
Likhith <semalalikithsai@gmail.com>
2026-02-17 21:20:15 +0530

    Apply formatting fixes from pre-commit hooks

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ import json
 import sys
 from typing import Any, Dict
 
-from config.model_cache import initialize_models
-
 from ads.matcher import AdMatchingRequest, AdMetadata, match_ads
+from config.model_cache import initialize_models
 from core.schema import UniversalIntent
 from extraction.extractor import IntentExtractionRequest, extract_intent
 from ranking.ranker import RankingRequest, SearchResult, rank_results
